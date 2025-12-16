43f541ba3df34e3e07f3f8a014599467ecd6818d
KozyrinSemen <garbagemilitia@gmail.com>
2025-12-17 00:47:29 +0900

    Build fix and test hook

--- Client/Test.cs
+++ Client/Test.cs
@@ -1,5 +1,11 @@
 ﻿namespace equation_trainer.Client;
 
 public class Test {
-  
+  public static void Start()
+  {
+    var node = new OperationNode(new OperationNode(new ValueNode(3), new ValueNode(4), new Addition()),
+      new ValueNode(5), new Multiplication());
+    Console.WriteLine(node);
+    Console.WriteLine(node.Value);
+  }
 }
--- obj/Debug/net9.0-windows/equation-trainer.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/equation-trainer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("equation-trainer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+520b766ebd1ed321ae26ae3db1d72cdbe8c3154d")]
 [assembly: System.Reflection.AssemblyProductAttribute("equation-trainer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("equation-trainer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
