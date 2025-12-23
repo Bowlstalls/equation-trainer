b31d0b9589f91b2c7f80118b4f9df7225b577c0a
Kozyrin Semen <garbagemilitia@gmail.com>
2025-12-23 22:21:09 +0300

    Rewrote equation generator to use the new polynomial class

--- Client/EquationGenerator.cs
+++ Client/EquationGenerator.cs
@@ -1,42 +1,25 @@
 ﻿namespace Client;
 
-public class EquationGenerator(EquationGenerator.Range root, EquationGenerator.Range multiplier) {
-  public Range Root = root;
-  public Range Multiplier = multiplier;
+public class EquationGenerator {
+  public Range Roots = new Range(-5, 5);
+  public Range Values = new Range(-10, 10);
+  public Range Multipliers = new Range(-5, 5);
   private Random _rand = new Random();
 
   public Equation Generate(int power)
   {
     var roots = GetRandomRoots(power);
-    var polynomial = GetPolynomial(power, roots);
-    return new Equation("", polynomial, roots);
+    var multiplier = _rand.Next(Multipliers.Min, Multipliers.Max);
+    var polynomial = new Polynomial(multiplier, roots);
+    return new Equation("", polynomial.ToArray(), roots);
   }
 
   private int[] GetRandomRoots(int power)
   {
     var res = new int[power];
     for (var i = 0; i < power; i++) {
-      res[i] = _rand.Next(root.Min, root.Max);
+      res[i] = _rand.Next(Roots.Min, Roots.Max);
     }
-    return res;
-  }
-  
-  private int[] GetPolynomial(int power, int[] roots)
-  {
-    var res = new int[power + 1];
-    var n = _rand.Next(Multiplier.Min, Multiplier.Max);
-    n = 1;
-    res[0] = n;
-    n *= -1;
-    for (var i = 0; i < power - 1; i++) {
-      var x = 0;
-      for (var j = i; j < power; j++) {
-        x += n * roots[j];
-      }
-      res[i + 1] = x;
-      n *= -roots[i];
-    }
-    res[power] = n * roots[power - 1];
     return res;
   }
   
