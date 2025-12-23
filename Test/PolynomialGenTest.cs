ec5aae1146fa3e5b0383f17f1ee9bdc65abf2cb4
Kozyrin Semen <garbagemilitia@gmail.com>
2025-12-23 22:21:23 +0300

    fixed unit test

--- Test/PolynomialGenTest.cs
+++ Test/PolynomialGenTest.cs
@@ -3,18 +3,28 @@
 namespace Test;
 
 public class PolynomialGenTest {
-  private EquationGenerator _gen = new EquationGenerator(new EquationGenerator.Range(-10, 10), new EquationGenerator.Range(-10, 10));
+  private EquationGenerator _gen = new EquationGenerator();
   
   [Test]
   [Repeat(100)]
   public void CheckValid()
   {
-    var pwr = 2;
+    const int pwr = 14;
     var eq = _gen.Generate(pwr);
-    double val = eq.Polynomial[pwr];
-    for (var i = 0; i < pwr; i++) {
-      val += Math.Pow(eq.Roots[i], pwr - i) * eq.Polynomial[pwr];
+    var broke = false;
+    for (var rootNum = 0; rootNum < pwr; rootNum++) {
+      double val = eq.Polynomial[pwr];
+      var root = eq.Roots[rootNum];
+      for (var i = 0; i < pwr; i++) {
+        val += Math.Pow(root, pwr - i) * eq.Polynomial[i];
+      }
+
+      if (val != 0) {
+        broke = true;
+        break;
+      }
     }
-    Assert.That(val, Is.EqualTo(0));
+    
+    Assert.That(!broke);
   }
 }
